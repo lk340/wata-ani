7534521de1756e5b1c9d45e60f798fe910d37294
lk340 <loyd.hkim@gmail.com>
2020-05-13 20:29:07 -0400

    :sparkles: [Back End] Users
    Add API endpoints for user CRUD.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+
+User = get_user_model()
+
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = serializers.UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+    def get_user(self, pk):
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializer = serializers.UserSerializer(user)
+        return Response(serializer.data)
+
+    def patch(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializer = serializers.UserSerializer(user, data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_user(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
