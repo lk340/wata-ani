da4c8f24d7100616ee495f67549d86a62be536d2
Loyd Kim <loyd.hkim@gmail.com>
2020-08-27 10:41:59 -0400

    :heavy_plus_sign: [Front End] UUID

--- frontend/src/components/auth-form/errors/errors.styled.tsx
+++ frontend/src/components/auth-form/errors/errors.styled.tsx
@@ -1,10 +1,16 @@
-import styled, { css, createGlobalStyle } from "styled-components";
+import styled, { css } from "styled-components";
 import { animated } from "react-spring";
+
+import * as Snippets from "@/utils/style/snippets";
 
 // ============== //
 // ↓↓↓ Errors ↓↓↓ //
 // ============== //
 
-export const Errors = styled("div")`
+export const Errors = styled(animated.div)`
 	border: red solid 1px;
+`;
+
+export const ErrorsText = styled("p")`
+	${Snippets.clearSpacing()};
 `;
--- frontend/src/components/auth-form/errors/index.tsx
+++ frontend/src/components/auth-form/errors/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
-
-import * as Context from "@/context";
+import * as ReactRedux from "react-redux";
 
 import * as Styled from "./errors.styled";
+import * as Springs from "./errors.springs";
 
 export const Errors = () => {
-  const { theme } = Context.Theme.useThemeContext();
-  
-  return (
-    <Styled.Errors>Errors</Styled.Errors>
-  );
+	const animateText = Springs.text();
+
+	const errors = ReactRedux.useSelector((state) => state.errors.session);
+
+	const errorMessages = errors.map((error: string) => {
+		return <Styled.ErrorsText key={}>{error}</Styled.ErrorsText>;
+	});
+
+	return <Styled.Errors style={animateText}>{errorMessages}</Styled.Errors>;
 };
