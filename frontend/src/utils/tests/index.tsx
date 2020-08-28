36ea3c322de482eee1ca89cf6ec08f0b45d17538
Loyd Kim <loyd.hkim@gmail.com>
2020-08-28 08:36:14 -0400

    :white_check_mark: [Front End] Test Utils
    Import Redux provider with store.

--- frontend/src/utils/tests/index.tsx
+++ frontend/src/utils/tests/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
+import * as ReactRedux from "react-redux";
 import * as Testing from "@testing-library/react";
 import * as Reach from "@reach/router";
 
 import { Providers } from "@/context/providers";
+import store from "@/redux/store";
 
 export function renderWithContext(component: React.ReactNode) {
-	return Testing.render(<Providers>{component}</Providers>);
+	return Testing.render(
+		<ReactRedux.Provider store={store()}>
+			<Providers>{component}</Providers>
+		</ReactRedux.Provider>,
+	);
 }
 
 export const WithLocation = (component: React.ReactNode) => {
