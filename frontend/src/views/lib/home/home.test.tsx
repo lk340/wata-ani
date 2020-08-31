b533d2364d9dc69bebfc370ab06ad66e806c9199
Loyd Kim <loyd.hkim@gmail.com>
2020-08-31 14:55:16 -0400

    :bug: [Front End] Home View
    Fix access to Redux store in end to end testing.

--- frontend/src/views/lib/home/home.test.tsx
+++ frontend/src/views/lib/home/home.test.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import * as ReactRedux from "react-redux";
 import * as Testing from "@testing-library/react";
 
 import { Providers } from "@/context/providers";
+import store from "@/redux/store";
 
 import { Home } from "./home";
 
@@ -11,8 +13,28 @@ function renderWithContext(component: React.ReactNode) {
 
 describe("Home Component", () => {
 	it("matches the snapshot", () => {
-		const { asFragment } = renderWithContext(<Home />);
+		const { asFragment } = renderWithContext(
+			<ReactRedux.Provider store={store()}>
+				<Home />
+			</ReactRedux.Provider>,
+		);
 
 		expect(asFragment(<Home />)).toMatchSnapshot();
+	});
+
+	it("renders the navbar component", () => {
+		const home = renderWithContext(
+			<ReactRedux.Provider store={store()}>
+				<Home />
+			</ReactRedux.Provider>,
+		);
+	});
+
+	it("renders the navbar mobile component", () => {
+		const home = renderWithContext(
+			<ReactRedux.Provider store={store()}>
+				<Home />
+			</ReactRedux.Provider>,
+		);
 	});
 });
