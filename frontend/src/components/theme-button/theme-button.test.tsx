6b651abe9a26de0890fc966a4fded097f3c0d922
Loyd Kim <loyd.hkim@gmail.com>
2020-08-31 14:47:56 -0400

    :white_check_mark: [Front End] Theme Button Component
    Add tests.

---
+++ frontend/src/components/theme-button/theme-button.test.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import * as Testing from "@testing-library/react";
+
+import * as Context from "@/context";
+import * as Tests from "@/utils/tests";
+
+import { ThemeButton } from "./theme-button";
+
+describe("ThemeButton Component", () => {
+	it("matches the snapshot", () => {
+		const { asFragment } = Tests.renderWithContext(<ThemeButton />);
+
+		expect(asFragment(<ThemeButton />)).toMatchSnapshot();
+	});
+
+	it("toggles the theme when the user clicks on it", () => {
+		const ThemeButtonWithMode = () => {
+			const { theme } = Context.Theme.useThemeContext();
+
+			return (
+				<>
+					<ThemeButton />
+					<div data-testid="mode">{theme.state.mode}</div>
+				</>
+			);
+		};
+
+		const themeButtonWithMode = Tests.renderWithContext(<ThemeButtonWithMode />);
+
+		const mode = themeButtonWithMode.getByTestId(/mode/);
+		expect(mode).toHaveTextContent(/light/);
+
+		const themeButton = themeButtonWithMode.getByTestId(/theme button component/);
+		Testing.fireEvent.click(themeButton);
+		expect(mode).toHaveTextContent(/dark/);
+	});
+});
