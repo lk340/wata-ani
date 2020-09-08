d015d939e61737b42781cb7a7c3771591ce8ab54
Loyd Kim <loyd.hkim@gmail.com>
2020-09-08 15:44:34 -0400

    :dizzy: [Front End] Home Page
    Add scroll reveal animation to CTA section.

--- frontend/src/views/lib/home/CTA/index.tsx
+++ frontend/src/views/lib/home/CTA/index.tsx
@@ -10,16 +10,18 @@ export const CTA = () => {
 
 	return (
 		<Styled.CTA style={animateCTA}>
-			<Styled.CTAContainer>
-				<Styled.CTACopy>
-					<Styled.CTACopyTitle>Become A Member Of The Community</Styled.CTACopyTitle>
-					<Styled.CTACopyBody>
-						Sign up to become a member of the community and gain member perks, such as
-						being able to write reviews, favorite reviews, rate reviews, and more!
-					</Styled.CTACopyBody>
-				</Styled.CTACopy>
-				<Components.CTAButton to="/registration" text="Get Started" />
-			</Styled.CTAContainer>
+			<Components.RevealOnScroll yOffset={-10}>
+				<Styled.CTAContainer>
+					<Styled.CTACopy>
+						<Styled.CTACopyTitle>Become A Member Of The Community</Styled.CTACopyTitle>
+						<Styled.CTACopyBody>
+							Sign up to become a member of the community and gain member perks, such as
+							being able to write reviews, favorite reviews, rate reviews, and more!
+						</Styled.CTACopyBody>
+					</Styled.CTACopy>
+					<Components.CTAButton to="/registration" text="Get Started" />
+				</Styled.CTAContainer>
+			</Components.RevealOnScroll>
 		</Styled.CTA>
 	);
 };
