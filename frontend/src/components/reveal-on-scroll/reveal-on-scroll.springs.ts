818040ee2dc9e45ac94862454292cd38a62ede3b
Loyd Kim <loyd.hkim@gmail.com>
2020-09-08 15:40:45 -0400

    :dizzy: [Front End] Home Page
    Add intro animation to reviews section.

--- frontend/src/components/reveal-on-scroll/reveal-on-scroll.springs.ts
+++ frontend/src/components/reveal-on-scroll/reveal-on-scroll.springs.ts
@@ -3,12 +3,12 @@ import * as Spring from "react-spring";
 export function visibility(isVisible: boolean, yOffset: number = 10, delay: number = 0) {
 	return Spring.useSpring({
 		from: {
-			opacity: "0",
 			transform: `translateY(${yOffset}px)`,
+			opacity: "0",
 		},
 		to: {
-			opacity: isVisible ? "1" : "0",
 			transform: isVisible ? "translateY(0px)" : `translateY(${yOffset}px)`,
+			opacity: isVisible ? "1" : "0",
 		},
 		delay,
 	});
--- frontend/src/views/lib/home/reviews/index.tsx
+++ frontend/src/views/lib/home/reviews/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import * as Components from "@/components";
+
 import * as Styled from "./reviews.styled";
 import * as Springs from "./reviews.springs";
 
@@ -10,26 +12,30 @@ export const Reviews = () => {
 		<Styled.Reviews>
 			{/* Images */}
 			<Styled.ReviewsImageContainer>
-				<Styled.ReviewsImageLight mode={localStorage.mode} />
-				<Styled.ReviewsImageDark mode={localStorage.mode} />
+				<Components.RevealOnScroll yOffset={-10} delay={150}>
+					<Styled.ReviewsImageLight mode={localStorage.mode} />
+					<Styled.ReviewsImageDark mode={localStorage.mode} />
+				</Components.RevealOnScroll>
 			</Styled.ReviewsImageContainer>
 
 			{/* Copy */}
 			<Styled.ReviewsCopy>
-				{/* Title */}
-				<Styled.ReviewsCopyTitle>Reviews</Styled.ReviewsCopyTitle>
-				{/* Body */}
-				<Styled.ReviewsCopyBodyContainer>
-					<Styled.ReviewsCopyBody style={animateBody}>
-						WataAni’s one paragraph limit for reviews is largely inspired by the words of
-						the late Albert Einstein: “If you can’t explain it simply, you don’t
-						understand it well enough.”
-					</Styled.ReviewsCopyBody>
-					<Styled.ReviewsCopyBody style={animateBody}>
-						WataAni wants to encourage its users to really think about the best ways they
-						can make reviews as succinctly as they can.
-					</Styled.ReviewsCopyBody>
-				</Styled.ReviewsCopyBodyContainer>
+				<Components.RevealOnScroll yOffset={-10}>
+					{/* Title */}
+					<Styled.ReviewsCopyTitle>Reviews</Styled.ReviewsCopyTitle>
+					{/* Body */}
+					<Styled.ReviewsCopyBodyContainer>
+						<Styled.ReviewsCopyBody style={animateBody}>
+							WataAni’s one paragraph limit for reviews is largely inspired by the words
+							of the late Albert Einstein: “If you can’t explain it simply, you don’t
+							understand it well enough.”
+						</Styled.ReviewsCopyBody>
+						<Styled.ReviewsCopyBody style={animateBody}>
+							WataAni wants to encourage its users to really think about the best ways
+							they can make reviews as succinctly as they can.
+						</Styled.ReviewsCopyBody>
+					</Styled.ReviewsCopyBodyContainer>
+				</Components.RevealOnScroll>
 			</Styled.ReviewsCopy>
 		</Styled.Reviews>
 	);
