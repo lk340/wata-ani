6f533802910004ed6ed7059c5a007e0d16aee096
lk340 <loyd.hkim@gmail.com>
2020-10-14 11:00:32 -0400

    :construction: [Front End] Profile Page
    Start.

---
+++ frontend/src/views/pages/profile.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import * as Wrappers from "@/wrappers";
+import * as Components from "@/components";
+import * as Lib from "@/views/lib";
+
+export default () => (
+	<Wrappers.Layout>
+		<Components.SEO title="Profile Page" />
+		{/* <Lib.Home /> */}
+		<div>Profile Page</div>
+	</Wrappers.Layout>
+);
