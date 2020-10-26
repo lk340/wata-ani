be4ccc2c60648520e818b8aecb29e565aa412c31
lk340 <loyd.hkim@gmail.com>
2020-10-26 11:28:40 -0400

    :construction: [Front End] Loading Animation
    WIP

--- frontend/src/components/loading-animation/loading-animation.springs.ts
+++ frontend/src/components/loading-animation/loading-animation.springs.ts
@@ -1,8 +1,17 @@
 import * as Spring from "react-spring";
 
 export function loading() {
-	return Spring.useSpring({
+	return Spring.useTransition(null, null, {
 		from: { opacity: "0" },
-		to: { opacity: "1" },
+		enter: { opacity: "1" },
+		leave: { opacity: "0" },
+		config: { duration: 0 },
+	});
+}
+
+export function circle() {
+	return Spring.useSpring({
+		from: { Animation },
+		to: { Animation },
 	});
 }
--- frontend/src/components/loading-animation/loading-animation.styled.tsx
+++ frontend/src/components/loading-animation/loading-animation.styled.tsx
@@ -1,4 +1,22 @@
 import styled, { css, createGlobalStyle } from "styled-components";
 import { animated } from "react-spring";
 
-export const Loading = styled("div")``;
+import * as Snippets from "@/utils/style/snippets";
+
+export const Loading = styled("div")`
+	${Snippets.fixed("0", "0", "0", "0")};
+
+	border: red solid 1px;
+`;
+
+export const LoadingCircle = styled("div")`
+	${Snippets.absoluteCenter()};
+`;
+
+export const LoadingCircleOuter = styled("div")`
+	${Snippets.makeCircle()};
+`;
+
+export const LoadingCircleInner = styled("div")`
+	${Snippets.makeCircle()};
+`;
